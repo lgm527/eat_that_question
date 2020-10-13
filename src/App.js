d91b694bd5fde63b4baf699da8cb4a95dc735bd1
lgm527 <laurellmccaffrey@gmail.com>
2020-10-13 14:30:08 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Eat That Question
-          <br></br>
+          <br/>
           <span id="puncuation"></span>
         </h1>
         <div id="play" onClick={() => window.alert("Let's do it!")}>YES</div>
@@ -16,6 +16,8 @@ function App() {
         src="https://cdn.jsdelivr.net/gh/MakeWeirdMusic/art@master/frank-zappa/frank-zappa.png"
         id="zappaSVG" 
         alt="zappa" />
+
+        <footer> <p>Made with&nbsp;🤯&nbsp;&nbsp;&&nbsp;🔮&nbsp;&nbsp;by <a href="https://github.com/lgm527/" target="_blank" rel="noopener noreferrer">@lgm527</a></p></footer>
     </div>
   );
 }
