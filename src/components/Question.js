9ec6a883368030a13fe63de15ebd26bb70d7c216
lgm527 <laurellmccaffrey@gmail.com>
2020-10-15 15:44:23 -0400

    question and answer layout and styles added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import '../styles/Question.css';
 
 const Question = () => {
     return ( 
         <div>
-            <h3>Watch out where the huskies go...</h3>
+            <h3 id="question">Watch out where the huskies go...</h3>
             <div className="answers">
-                <input type="radio" value="And don't you eat that yellow snow" /> And don't you eat that yellow snow
-                <input type="radio" value="But shouldn't you already know" /> But shouldn't you already know
-                <input type="radio" value="Could you really believe that" /> Could you really believe that
+                <div id="A">
+                    <input type="radio" value="And don't you eat that yellow snow" /> And don't you eat that yellow snow
+                </div>
+                <div id="B">
+                    <input type="radio" value="But shouldn't you already know" /> But shouldn't you already know
+                </div>
+                <div id="C">
+                    <input type="radio" value="Could you really believe that" /> Could you really believe that
+                </div>   
             </div>
         </div>
      );
