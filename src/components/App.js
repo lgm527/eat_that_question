27854a08bd69752c8ba20d0e1dc638cc982dca66
lgm527 <laurellmccaffrey@gmail.com>
2020-11-11 12:15:02 -0500

    main layout corrected

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
 
       return ( 
         <div className="App">
-        <header>
+        <header className="App-header">
           <h1>Eat That Question
           {
               this.props.playing ?
@@ -49,14 +49,12 @@ class App extends React.Component {
           </h1>
         </header>
   
-        <div className="main">
         {
           this.props.playing ?
               questions[this.props.index]
             :
             <Landing score={this.props.score} playing={this.playing} />
         }
-        </div>
   
         <footer> <p>Made with&nbsp;<span role="img" aria-label="billy the mountain emoji">🏔</span>&nbsp;&&nbsp;<span role="img" aria-label="crystal ball emoji">🔮</span>&nbsp;&nbsp;by <a href="https://github.com/lgm527/" target="_blank" rel="noopener noreferrer">@lgm527</a></p></footer>
       </div>
